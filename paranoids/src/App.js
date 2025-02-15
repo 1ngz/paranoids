e51b5c9ee88610be584477b15bc9c61575a77900
ingz <dlwlgur7@naver.com>
2025-02-15 17:14:34 +0900

    commit
    commmit

--- paranoids/src/App.js
+++ paranoids/src/App.js
@@ -3,12 +3,19 @@ import Logo from "./Logo";
 import './App.css';
 import './layout.css';
 
+import { BrowserRouter } from "react-router-dom";
+
+
 const App = () => {
   return (
+
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    
     <div className="background-image">
       <Logo />
       <div className="comingSoon">C o m i n g S o o n </div>
     </div>
+    </BrowserRouter>
   );
 };
 
